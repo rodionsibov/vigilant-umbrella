7666c286b4040e87d467927a070623848f1a9e66
Rodion Sibov <rodionsibov@gmail.com>
2023-03-01 09:10:03 +0200

    Show the HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,9 +8,8 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  selectedHero?: Hero;
-
   heroes = HEROES;
+  selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
